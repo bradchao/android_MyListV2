987e89f91b9cf0e0e2d8718a455f967b3b124f91
Brad Chao <brad@brad.tw>
2016-09-22 09:32:17 +0800

    v9

--- app/src/main/java/brad/tw/mylistv2/List2Activity.java
+++ app/src/main/java/brad/tw/mylistv2/List2Activity.java
@@ -2,7 +2,10 @@ package brad.tw.mylistv2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import java.util.LinkedList;
 
@@ -10,6 +13,7 @@ public class List2Activity extends AppCompatActivity {
     private RecyclerView recycler;
     private String[] dataset = {"Item 1","Item 2","Item 3","Item 4","Item 5","Item 6"};
     private LinkedList<String> data;
+    private LinearLayoutManager mLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,5 +27,44 @@ public class List2Activity extends AppCompatActivity {
 
         recycler = (RecyclerView)findViewById(R.id.recyclerView);
 
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        recycler.setLayoutManager(mLayoutManager);
+
+        // specify an adapter (see also next example)
+//        mAdapter = new MyAdapter(myDataset);
+//        recycler.setAdapter(mAdapter);
     }
+
+    private class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+
+        class MyViewHolder extends RecyclerView.ViewHolder {
+            // each data item is just a string in this case
+            public TextView mTextView;
+            public MyViewHolder(TextView v) {
+                super(v);
+                mTextView = v;
+            }
+        }
+
+        public MyAdapter() {
+        }
+
+        @Override
+        public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return null;
+        }
+
+        @Override
+        public void onBindViewHolder(MyViewHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return 0;
+        }
+    }
+
+
 }
